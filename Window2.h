eb46ef5e07562edf0fd4e80ee4d8decb38ba5c42
ClaraLuzCA <clara.cornejo@ingenieria.unam.edu>
2024-03-09 23:16:30 -0600

    commit

---
+++ Window2.h
@@ -0,0 +1,49 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getrotay() { return rotay; }
+	GLfloat getrotax() { return rotax; }
+	GLfloat getrotaz() { return rotaz; }
+	GLfloat getarticulacion1() { return articulacion1; }
+	GLfloat getarticulacion12() { return articulacion12; }
+	GLfloat getarticulacion2() { return articulacion2; }
+	GLfloat getarticulacion22() { return articulacion22; }
+	GLfloat getarticulacion3() { return articulacion3; }
+	GLfloat getarticulacion32() { return articulacion32; }
+	GLfloat getarticulacion4() { return articulacion4; }
+	GLfloat getarticulacion42() { return articulacion42; }
+	
+
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	GLfloat rotax,rotay,rotaz, articulacion1, articulacion12, articulacion2, articulacion22, articulacion3, articulacion32, articulacion4, articulacion42;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	bool mouseFirstMoved;
+	void createCallbacks();
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+};
+
